d4ca5da73cf381a6581b9cba85bd552ec18e2370
Samantha Le Huec <s.lhuec@gmail.com>
2023-05-19 19:23:39 +0200

    fix check day ongoing

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -40,6 +40,9 @@ void BitcoinExchange::reset_tokens()
 	_line = "";
 	_value = "";
 	_date = "";
+	_year = "";
+	_month = "";
+	_day = "";
 	_value_nb = 0;
 	_is_int = false;
 	_error_message = "";
@@ -87,9 +90,9 @@ void BitcoinExchange::split_date_in_tokens()
 		count_step += 1;
 	}
 
-	DEBUG("YEAR: ", _year);
-	DEBUG("MONTH: ", _month);
-	DEBUG("DAY: ", _day);
+	// DEBUG("YEAR: ", _year);
+	// DEBUG("MONTH: ", _month);
+	// DEBUG("DAY: ", _day);
 }
 
 bool BitcoinExchange::check_year()
@@ -100,7 +103,9 @@ bool BitcoinExchange::check_year()
 		std::istringstream str_to_conv(_year);
 		str_to_conv >> int_type;
 		if (int_type >= 2000 && int_type < 2030) // TODO verifier si ça correspond au comportement attendu par le prog
+		{
 			return true;
+		}
 		else
 		{
 			this->_error_message = "Error: bad input => " + _date;
@@ -124,7 +129,11 @@ bool BitcoinExchange::check_month()
 		if (int_type >= 1 && int_type < 13)
 		{
 			if (check_day(int_type))
+			{
+				
 				_day_ok = true;
+
+			}
 			return true;
 		}
 		else
@@ -146,12 +155,14 @@ bool BitcoinExchange::is_bissextile()
 	int int_type = 0;
 	std::istringstream str_to_conv(_year);
 	str_to_conv >> int_type;
-	if ((int_type % 4) && (int_type % 100) && (int_type % 400))
+
+	if ((int_type % 4 == 0) || (int_type % 100 == 0) || (int_type % 400 == 0))
 		return true;
 	else
 		return false;
 }
 
+// FIXME  NOT WORKING
 bool BitcoinExchange::check_day(int month)
 {
 	int int_day = 0;
@@ -163,7 +174,9 @@ bool BitcoinExchange::check_day(int month)
 		if (month == 4 || month == 6 || month == 9 || month == 11)
 		{
 			if (int_day >= 1 && int_day <= 30)
+			{
 				return true;
+			}
 			else
 			{
 				this->_error_message = "Error: bad input, day must be between 1 and 30 => " + _date;
@@ -180,9 +193,8 @@ bool BitcoinExchange::check_day(int month)
 				return false;
 			}
 		}
-		else if (month == 2)
+		else
 		{
-			//FIXME
 			if (is_bissextile())
 			{
 				if (int_day >= 1 && int_day <= 29)
@@ -205,7 +217,12 @@ bool BitcoinExchange::check_day(int month)
 			}
 		}
 	}
-	return true;
+	else
+	{
+		this->_error_message = "Error: bad input => " + _date;
+		return false;
+	}
+	// return true;
 }
 
 bool BitcoinExchange::date_format_is_valid()
@@ -215,11 +232,18 @@ bool BitcoinExchange::date_format_is_valid()
 	int ok = 0; // si ok == 3 return true sinon return false
 
 	if (check_year())
+	{
 		ok += 1;
+	}
 	if (check_month())
+	{
+
 		ok += 1;
+	}
 	if (_day_ok)
+	{
 		ok += 1;
+	}
 	if (ok == 3)
 		return true;
 	else
@@ -235,15 +259,19 @@ bool BitcoinExchange::value_is_valid()
 	std::istringstream str_to_conv(_value);
 
 	if (str_to_conv >> float_type)
+	{
 		_value_nb = float_type;
+	}
 	else
 	{
+
 		this->_error_message = "Error: bad input => " + _line;
 		return false;
 	}
 
 	if (_is_int)
 	{
+
 		if (_value_nb < 0)
 		{
 			_error_message = "Error: not a positive number.";
@@ -308,10 +336,15 @@ void BitcoinExchange::exchange_calculator(std::ifstream &input_file)
 		}
 		if (date_format_is_valid() && value_is_valid())
 		{
-			find_date();
+			find_date(); // TODO
 			calculate(); // TODO attention bien utiliser la var _value_nb pour les calculs !!
 		}
 		else
+		{
 			PRINT(_error_message);
+		}
 	}
 }
+
+// TODO vérifier que les lignes vides ont disparu apres avoir codé calculate and print
+// hint: cela vient  de reset token?
