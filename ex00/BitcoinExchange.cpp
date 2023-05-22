29d267bffabdaa4fd07233f1a86e5f5ef5fae396
Samantha Le Huec <s.lhuec@gmail.com>
2023-05-22 15:28:32 +0200

    ex00 done

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -5,16 +5,37 @@
 //==================================================================================================
 BitcoinExchange::BitcoinExchange() {} // non utilisé, mis en private
 
-BitcoinExchange::BitcoinExchange(std::ifstream &input_file) : _line(""), _date(""), _value(""), _value_nb(0), _is_int(false), _error_message("")
+BitcoinExchange::BitcoinExchange(std::ifstream &input_file) : _line(""),
+															  _date(""),
+															  _year(""),
+															  _month(""),
+															  _day(""),
+															  _day_ok(false),
+															  _value(""),
+															  _value_nb(0),
+															  _result(0),
+															  _is_int(false),
+															  _error_message("")
 {
-	save_csv_data(); // TODO save in map
+	save_csv_data();
+	// print_map();
 	exchange_calculator(input_file);
 }
 
-// BitcoinExchange::BitcoinExchange(BitcoinExchange const &cpy)
-// {
-
-// }
+BitcoinExchange::BitcoinExchange(BitcoinExchange const &cpy) : _line(cpy._line),
+															   _date(cpy._date),
+															   _year(cpy._year),
+															   _month(cpy._month),
+															   _day(cpy._day),
+															   _day_ok(cpy._day_ok),
+															   _value(cpy._value),
+															   _value_nb(cpy._value_nb),
+															   _result(cpy._result),
+															   _is_int(cpy._is_int),
+															   _error_message(cpy._error_message)
+{
+	*this = cpy;
+}
 
 BitcoinExchange::~BitcoinExchange()
 {
@@ -24,10 +45,24 @@ BitcoinExchange::~BitcoinExchange()
 //		operator overload
 //==================================================================================================
 
-// BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange const &rhs)
-// {
-
-// }
+BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange const &rhs)
+{
+	if (this != &rhs)
+	{
+		_line = rhs._line;
+		_date = rhs._date;
+		_year = rhs._year;
+		_month = rhs._month;
+		_day = rhs._day;
+		_day_ok = rhs._day_ok;
+		_value = rhs._value;
+		_value_nb = rhs._value_nb;
+		_result = rhs._result;
+		_is_int = rhs._is_int;
+		_error_message = rhs._error_message;
+	}
+	return *this;
+}
 
 //==================================================================================================
 //		members functions
@@ -46,6 +81,15 @@ void BitcoinExchange::reset_tokens()
 	_value_nb = 0;
 	_is_int = false;
 	_error_message = "";
+}
+
+void BitcoinExchange::print_map()
+{
+	std::map<std::string, float>::iterator it = _data_csv.begin();
+	for (; it != this->_data_csv.end(); it++)
+	{
+		std::cout << CYAN << "map content: " << it->first << " " << it->second << RES << std::endl;
+	}
 }
 
 // parsing input file_______________________________________________________________________________
@@ -89,10 +133,9 @@ void BitcoinExchange::split_date_in_tokens()
 		tmp_date.erase(tmp_date.begin(), tmp_date.begin() + (pos + 1));
 		count_step += 1;
 	}
-
-	// DEBUG("YEAR: ", _year);
-	// DEBUG("MONTH: ", _month);
-	// DEBUG("DAY: ", _day);
+	// INFO("YEAR: ", _year);
+	// INFO("MONTH: ", _month);
+	// INFO("DAY: ", _day);
 }
 
 bool BitcoinExchange::check_year()
@@ -102,10 +145,8 @@ bool BitcoinExchange::check_year()
 	{
 		std::istringstream str_to_conv(_year);
 		str_to_conv >> int_type;
-		if (int_type >= 2000 && int_type < 2030) // TODO verifier si ça correspond au comportement attendu par le prog
-		{
+		if (int_type >= 2000 && int_type < 2030)
 			return true;
-		}
 		else
 		{
 			this->_error_message = "Error: bad input => " + _date;
@@ -129,11 +170,9 @@ bool BitcoinExchange::check_month()
 		if (int_type >= 1 && int_type < 13)
 		{
 			if (check_day(int_type))
-			{
-				
 				_day_ok = true;
-
-			}
+			else
+				return false;
 			return true;
 		}
 		else
@@ -161,7 +200,6 @@ bool BitcoinExchange::is_bissextile()
 		return false;
 }
 
-// FIXME  NOT WORKING dans le check affiné des dates
 bool BitcoinExchange::check_day(int month)
 {
 	int int_day = 0;
@@ -173,9 +211,7 @@ bool BitcoinExchange::check_day(int month)
 		if (month == 4 || month == 6 || month == 9 || month == 11)
 		{
 			if (int_day >= 1 && int_day <= 30)
-			{
 				return true;
-			}
 			else
 			{
 				this->_error_message = "Error: bad input, day must be between 1 and 30 => " + _date;
@@ -221,28 +257,20 @@ bool BitcoinExchange::check_day(int month)
 		this->_error_message = "Error: bad input => " + _date;
 		return false;
 	}
-	// return true;
 }
 
 bool BitcoinExchange::date_format_is_valid()
 {
 	split_date_in_tokens();
 
-	int ok = 0; // si ok == 3 return true sinon return false
+	int ok = 0;
 
 	if (check_year())
-	{
 		ok += 1;
-	}
 	if (check_month())
-	{
-
 		ok += 1;
-	}
 	if (_day_ok)
-	{
 		ok += 1;
-	}
 	if (ok == 3)
 		return true;
 	else
@@ -258,12 +286,9 @@ bool BitcoinExchange::value_is_valid()
 	std::istringstream str_to_conv(_value);
 
 	if (str_to_conv >> float_type)
-	{
 		_value_nb = float_type;
-	}
 	else
 	{
-
 		this->_error_message = "Error: bad input => " + _line;
 		return false;
 	}
@@ -292,29 +317,82 @@ void BitcoinExchange::split_and_get_val()
 
 	pos = _line.find('|', i);
 	_date = _line.substr(i, (pos - i - 1));
-	DEBUG("date is: ", _date);
+	// INFO("date is: ", _date);
+
 	i = pos + 2; // saute le space
 	_value = _line.substr((i), ((_line.size())) - i);
-	DEBUG("value is: ", _value);
+	// INFO("value is: ", _value);
 }
 
 // get datas and values_______________________________________________________________________________
 
+void BitcoinExchange::split_in_pair(std::string const &entry_line)
+{
+	size_t pos;
+	size_t i = 0;
+	std::string key;
+	std::string val;
+
+	pos = entry_line.find(',', i);
+	key = entry_line.substr(i, (pos - i));
+	// INFO("key date is: ", key);
+
+	i = pos + 1;
+	val = entry_line.substr((i), ((entry_line.size())) - i);
+	// INFO("value is: ", val);
+
+	std::istringstream val_to_conv(val);
+	float float_type;
+	val_to_conv >> float_type;
+	// INFO("value after conversion is: ", float_type);
+
+	this->_data_csv[key] = float_type; // ds map: si a la pos [] il n'y a rien, la pair key val est inseree
+}
+
 void BitcoinExchange::save_csv_data()
 {
-	INFO("Here, save csv datas", "");
+	// get line avec ifstream
+	std::string entry_line;
+	std::ifstream csv_file("./data.csv", std::ifstream::in);
+
+	if (csv_file.good())
+	{
+		while (!csv_file.eof())
+		{
+			std::getline(csv_file, entry_line);
+			split_in_pair(entry_line); // ici save ds map
+		}
+	}
+	else
+	{
+		ERROR("Error: cannot open and read csv file.");
+		return;
+	}
 }
 
 // output result_______________________________________________________________________________
 
 void BitcoinExchange::find_date()
 {
-	INFO("Here, find date in map csv data", "");
-}
+	std::map<std::string, float>::iterator it;
+	it = _data_csv.find(_date);
+	if (it == _data_csv.end())
+	{
+		it = _data_csv.lower_bound(_date);
+		it--;
 
-void BitcoinExchange::calculate()
-{
-	INFO("Here, calculate and print", "");
+		// DEBUG_______________________________________________________________
+		//  if (it != _data_csv.end())
+		//  	std::cout << CYAN << "lower key is: " << it->first << RES << std::endl;
+		//  else
+		//  DEBUG("key not found", "");
+		// FIN DU DEBUG_______________________________________________________
+	}
+	if (it != _data_csv.end())
+	{
+		_result = _value_nb * it->second;
+		std::cout << WHITE << _date << " => " << _value << " = " << _result << RES << std::endl;
+	}
 }
 
 // read file line by line in loop___________________________________________________________________
@@ -324,7 +402,7 @@ void BitcoinExchange::exchange_calculator(std::ifstream &input_file)
 	{
 		reset_tokens();
 		std::getline(input_file, _line);
-		ERROR(_line);
+		// INFO(_line, "");
 
 		if (line_format_is_valid())
 			split_and_get_val();
@@ -334,16 +412,10 @@ void BitcoinExchange::exchange_calculator(std::ifstream &input_file)
 			continue;
 		}
 		if (date_format_is_valid() && value_is_valid())
-		{
-			find_date(); // TODO
-			calculate(); // TODO attention bien utiliser la var _value_nb pour les calculs !!
-		}
+			find_date();
 		else
 		{
 			PRINT(_error_message);
 		}
 	}
 }
-
-// TODO vérifier que les lignes vides ont disparu apres avoir codé calculate and print
-// hint: cela vient  de reset token?
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -4,15 +4,14 @@
 class BitcoinExchange
 {
 public:
-	// TODO //coplian form_____________________________________________________________
+	//coplian form_____________________________________________________________
 
 	BitcoinExchange(std::ifstream &input_file);
-	// BitcoinExchange(BitcoinExchange const &cpy);
+	BitcoinExchange(BitcoinExchange const &cpy);
 	~BitcoinExchange();
 
-	// BitcoinExchange &operator=(BitcoinExchange const &rhs);
+	BitcoinExchange &operator=(BitcoinExchange const &rhs);
 
-	// function used in main____________________________________________________
 
 private:
 	//__________________________________members functions
@@ -20,6 +19,7 @@ private:
 
 	// utils
 	void reset_tokens();
+	void print_map();
 
 	// parsing- error handlers
 	bool line_format_is_valid();
@@ -33,29 +33,29 @@ private:
 
 	// get datas, set attributs
 	void save_csv_data();
+	void split_in_pair(std::string const& entry_line);
 	void split_and_get_val();
 
 	// finder/calculator
 	void find_date();
-	void calculate();
 
 	// read file
 	void exchange_calculator(std::ifstream &input_file);
 
 	//__________________________________attribute members
 
-	std::string _line;
-	std::string _date;
-	std::string _year;
-	std::string _month;
-	std::string _day;
-	bool		_day_ok;
-	std::string _value;
-	float 		_value_nb;
-	bool 		_is_int;
-	std::string _error_message;
-	std::map<std::string, float> _data_csv;
-
+	std::string						_line;
+	std::string						_date;
+	std::string						_year;
+	std::string						_month;
+	std::string						_day;
+	bool							_day_ok;
+	std::string						_value;
+	float 							_value_nb;
+	float 							_result;
+	bool 							_is_int;
+	std::string						_error_message;
+	std::map<std::string, float>	_data_csv;
 };
 
 #endif
