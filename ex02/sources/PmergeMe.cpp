1ea367cf0ddeb7f3c8e062e4a656ac6887946b7b
Samantha Le Huec <s.lhuec@gmail.com>
2023-06-20 14:25:37 +0200

    try

--- ex02/sources/PmergeMe.cpp
+++ ex02/sources/PmergeMe.cpp
@@ -132,16 +132,16 @@ void PmergeMe::insertion_sort(std::vector<int> &arr, int start, int end)
 {
 	for (int i = start + 1; i <= end; i++)
 	{
-		int key = arr[i];
+		int tmp = arr[i];
 		int j = i - 1;
 
-		while (j >= start && arr[j] > key)
+		while (j >= start && arr[j] > tmp)
 		{
 			arr[j + 1] = arr[j];
 			j--;
 		}
 
-		arr[j + 1] = key;
+		arr[j + 1] = tmp;
 	}
 }
 void PmergeMe::merge_insertion_sort(std::vector<int> &arr)
@@ -206,16 +206,16 @@ void PmergeMe::insertion_sort(std::deque<int> &arr, int start, int end)
 {
 	for (int i = start + 1; i <= end; i++)
 	{
-		int key = arr[i];
+		int tmp = arr[i];
 		int j = i - 1;
 
-		while (j >= start && arr[j] > key)
+		while (j >= start && arr[j] > tmp)
 		{
 			arr[j + 1] = arr[j];
 			j--;
 		}
 
-		arr[j + 1] = key;
+		arr[j + 1] = tmp;
 	}
 }
 void PmergeMe::merge_insertion_sort(std::deque<int> &arr)
